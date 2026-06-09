706d1f70b053e3f698dbd0ee691394b0340a0b61
Ramirez Marcos Elias <tecmorym0@gmail.com>
2026-06-09 13:46:30 -0300

    .2

--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -10,6 +10,7 @@ for (int i = 0; i < 3; i++)
     empleados[i].Apellido = Console.ReadLine();
     Console.WriteLine("Ingrese la fecha de nacimiento del empleado en formato yyyy/mm/dd: ");
     string fecha = Console.ReadLine();
+    // DateTime fechaAParsear = new DateTime(2004 , 4 , 13);
     DateTime fechaAParsear;
     bool parseoFecha = DateTime.TryParse(fecha, out fechaAParsear);
     empleados[i].FechaNacimiento = fechaAParsear;
@@ -19,7 +20,7 @@ for (int i = 0; i < 3; i++)
         Console.WriteLine("c. Casado");
         Console.WriteLine("s. soltero");
         Console.WriteLine("v. viudo");
-        empleados[i].EstadoCivil = Console.ReadLine()[0];
+        empleados[i].EstadoCivil = Console.ReadLine().First();
         if (empleados[i].EstadoCivil == 'c' || empleados[i].EstadoCivil == 's' || empleados[i].EstadoCivil == 'v')
         {
             salida = false;
@@ -29,17 +30,20 @@ for (int i = 0; i < 3; i++)
             Console.WriteLine("No ingreso un dato valido");
         }
     } while (salida);
-    double  sueldo;
-    double.TryParse(Console.ReadLine(), out sueldo); 
+    double sueldo;
+    double.TryParse(Console.ReadLine(), out sueldo);
     empleados[i].Sueldo = sueldo;
-    Console.WriteLine("ingrese una opcion para almacenar el cargo que tiene: ");
-    Console.WriteLine("1.Auxiliar");
-    Console.WriteLine("2.Administrativo");
-    Console.WriteLine("3.Ingeniero");
-    Console.WriteLine("4.Especialista");
-    Console.WriteLine("5.Investigador");
     int cargoQueTiene;
-    int.TryParse(Console.ReadLine(),out cargoQueTiene);
-    empleados[i].Cargo = cargoQueTiene;
+    do
+    {
+        Console.WriteLine("ingrese una opcion para almacenar el cargo que tiene: ");
+        Console.WriteLine("1.Auxiliar");
+        Console.WriteLine("2.Administrativo");
+        Console.WriteLine("3.Ingeniero");
+        Console.WriteLine("4.Especialista");
+        Console.WriteLine("5.Investigador");
+        int.TryParse(Console.ReadLine(), out cargoQueTiene);
+    } while (cargoQueTiene < 1 && cargoQueTiene > 5);
+    empleados[i].Cargo = (Cargos)cargoQueTiene;
 }
 
