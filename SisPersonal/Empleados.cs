5709efc06208a5d71d79786b453108ba0e595529
Ramirez Marcos Elias <tecmorym0@gmail.com>
2026-06-09 13:25:05 -0300

    ejercicio 2 casi terminado

--- SisPersonal/Empleados.cs
+++ SisPersonal/Empleados.cs
@@ -1,29 +1,78 @@
-public class Empleado
+namespace Empleados
 {
-    private string nombre;
-    private string apellido;
-    private DateTime fechaNacimiento;
-    private char estadoCivil;
-    private DateTime fechaDeIngreso;
-    private double sueldo;
 
-    public string Nombre { get => nombre; set => nombre = value; }
-    public string Apellido { get => apellido; set => apellido = value; }
-    public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
-    public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
-    public DateTime FechaDeIngreso1 { get => fechaDeIngreso; set => fechaDeIngreso = value; }
-    public double Sueldo { get => sueldo; set => sueldo = value; }
 
-    enum Cargos{
+    public class Empleado
+    {
+        private string nombre;
+        private string apellido;
+        private DateTime fechaNacimiento;
+        private char estadoCivil;
+        private DateTime fechaDeIngreso;
+        private double sueldo;
+        private Cargos cargo;
+
+        public string Nombre { get => nombre; set => nombre = value; }
+        public string Apellido { get => apellido; set => apellido = value; }
+        public DateTime FechaNacimiento { get => fechaNacimiento; set => fechaNacimiento = value; }
+        public char EstadoCivil { get => estadoCivil; set => estadoCivil = value; }
+        public DateTime FechaDeIngreso1 { get => fechaDeIngreso; set => fechaDeIngreso = value; }
+        public double Sueldo { get => sueldo; set => sueldo = value; }
+        public Cargos Cargo { get => cargo; set => cargo = value; }
+
+        public int calcularAntiguedad()
+        {
+            return DateTime.Now.Year - fechaDeIngreso.Year;
+        }
+        public int calcularEdad()
+        {
+            return DateTime.Now.Year - fechaNacimiento.Year;
+        }
+        public int aniosParaJubilarse()
+        {
+            if (calcularEdad() < 65)
+            {
+                return 65 - calcularEdad();
+            }
+            else if (calcularEdad() == 65)
+            {
+                return 0;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public double calcularSalario()
+        {
+            double adicional, salario;
+            if (calcularAntiguedad() <= 20)
+            {
+                adicional = sueldo * (calcularAntiguedad() * 0.01);
+            }
+            else
+            {
+                adicional = sueldo * 0.25;
+            }
+            if (cargo == Cargos.Ingeniero || cargo == Cargos.Especialista)
+            {
+                adicional *= 1.50; 
+            }
+            if (estadoCivil == 'C')
+            {
+                adicional += 150000; 
+            }
+            salario = sueldo + adicional;
+            return salario;
+        }
+    }
+    public enum Cargos
+    {
         Auxiliar = 1,
         Administrativo = 2,
         Ingeniero = 3,
         Especialista = 4,
         Investigador = 5,
-    }   
-
-    public int calcularAntiguedad()
-    {
-        return DateTime.Now.Year - fechaDeIngreso.Year; 
     }
+
 }
--- SisPersonal/Program.cs
+++ SisPersonal/Program.cs
@@ -1,1 +1,45 @@
-﻿
+﻿using Empleados;
+Empleado[] empleados = new Empleado[3];
+bool salida = true;
+for (int i = 0; i < 3; i++)
+{
+    empleados[i] = new Empleado();
+    Console.WriteLine("Ingrese el Nombre del empleado: ");
+    empleados[i].Nombre = Console.ReadLine();
+    Console.WriteLine("Ingrese el apellido del empleado: ");
+    empleados[i].Apellido = Console.ReadLine();
+    Console.WriteLine("Ingrese la fecha de nacimiento del empleado en formato yyyy/mm/dd: ");
+    string fecha = Console.ReadLine();
+    DateTime fechaAParsear;
+    bool parseoFecha = DateTime.TryParse(fecha, out fechaAParsear);
+    empleados[i].FechaNacimiento = fechaAParsear;
+    do
+    {
+        Console.WriteLine("Ingrese el estado civil del empleado: " + empleados[i].Nombre);
+        Console.WriteLine("c. Casado");
+        Console.WriteLine("s. soltero");
+        Console.WriteLine("v. viudo");
+        empleados[i].EstadoCivil = Console.ReadLine()[0];
+        if (empleados[i].EstadoCivil == 'c' || empleados[i].EstadoCivil == 's' || empleados[i].EstadoCivil == 'v')
+        {
+            salida = false;
+        }
+        else
+        {
+            Console.WriteLine("No ingreso un dato valido");
+        }
+    } while (salida);
+    double  sueldo;
+    double.TryParse(Console.ReadLine(), out sueldo); 
+    empleados[i].Sueldo = sueldo;
+    Console.WriteLine("ingrese una opcion para almacenar el cargo que tiene: ");
+    Console.WriteLine("1.Auxiliar");
+    Console.WriteLine("2.Administrativo");
+    Console.WriteLine("3.Ingeniero");
+    Console.WriteLine("4.Especialista");
+    Console.WriteLine("5.Investigador");
+    int cargoQueTiene;
+    int.TryParse(Console.ReadLine(),out cargoQueTiene);
+    empleados[i].Cargo = cargoQueTiene;
+}
+
